0e70efea637e402d503e8b665713c0ddf3dad21a
erikasousaa <80004644+erikasousaa@users.noreply.github.com>
2021-11-11 14:39:47 -0300

    Add files via upload

---
+++ projetodjango/urls.py
@@ -0,0 +1,10 @@
+
+from django.contrib import admin
+from django.urls import path, include
+
+from apps.home import urls as home_urls
+
+urlpatterns = [
+    path('home/', include(home_urls)),
+    path('admin/', admin.site.urls),
+]
